e80d5c8351a02cbab8575623ded059b476c3d346
G0rz <leonardo.becerril@hotmail.com>
2025-11-16 16:23:02 -0600

    feat: request for images of stock

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -11,7 +11,7 @@ export const ProductGridItem = ({product}: Props) => {
     return (
         <div className="rounded-md shadow-2xl overflow-hidden fade-in">
             <Image width={500} height={500} loading="eager"
-                   src={`https://api.scryfall.com/cards/${product.image}/?format=image`} alt={product.name} className="w-full object-cover"/>
+                   src={`https://api.scryfall.com/cards/${product.image}/?format=image`} alt={`${product.name}`} className="w-full object-cover"/>
         </div>
     )
 }
