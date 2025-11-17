215aca0aec1ae6ee3c5e552edbbe627eb1d3af1c
G0rz <leonardo.becerril@hotmail.com>
2025-11-16 18:03:35 -0600

    feat: Creating cards layout for items

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -10,18 +10,18 @@ interface Props{
 export const ProductGridItem = ({product}: Props) => {
 
     return (
-        <div className="relative rounded-md shadow-2xl overflow-hidden fade-in hover:scale-125 hover:z-10">
+        <div className="relative rounded-lg overflow-hidden fade-in hover:scale-125 hover:z-10 text-sm md:text-md">
             <Link href={`/product/${product.slug}`}>
             <Image width={500} height={500} loading="eager"
                    src={`https://api.scryfall.com/cards/${product.image}/?format=image`} alt={`${product.name}`}
                    className="w-full object-cover"/>
                 <div
-                    className="absolute bottom-0 w-full px-2 py-1 flex flex-col text-foreground backdrop-blur-md bg-black/20 text-xs">
+                    className="absolute bottom-0 backdrop-blur-md bg-black/20 w-full px-2 py-1 flex flex-col text-foreground text-shadow-lg">
                     <Link href={`/product/${product.slug}`}>
-                        <span className=" hover:text-primary"> {product.name} ({product.foil}) </span>
+                        <span className="font-bold hover:text-blue-600"> {product.name} ({product.foil}) </span>
                     </Link>
-                    <span className="font-bold">${product?.price}</span>
-                    <span className="font-bold">In Stock: {product?.quantity}</span>
+                    <span className="">${product?.price === null ? "N/A" : product?.price}</span>
+                    <span className="">In Stock: {product?.quantity} ({product.condition === "near_mint" ? "NM" : product.condition})</span>
                 </div>
             </Link>
         </div>
